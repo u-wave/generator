d8e2822bb0dbcdff3fac50d04363cbe72532f82d
René Kooi <renee@kooi.me>
2016-09-12 10:13:06 +0200

    add sources+web-plugins config to server.js template

--- src/app/templates/server.js
+++ src/app/templates/server.js
@@ -1,9 +1,11 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import compression from 'compression';
 import express from 'express';
 import uwave from 'u-wave-core';
 import createWebApi from 'u-wave-api-v1';
 import createWebClient from 'u-wave-web';
+import mediaSources from './sources';
 
 const app = express();
 const server = app.listen(process.env.PORT || 6041);
@@ -13,16 +15,22 @@ const uw = uwave({
   mongo: '<%= mongo %>',
 });
 
+uw.use(mediaSources);
+
 const api = createWebApi(uw, {
   server,
   secret: fs.readFileSync(
-    path.join(__dirname, './config/api-v1-secret.dat')
+    path.join(__dirname, '../config/api-v1-secret.dat')
   ),
 });
 
 const client = createWebClient(uw, {
   apiUrl: '/v1',
+  pluginsScriptFile: path.join(__dirname, './webPlugins.js'),
+  pluginsStyleFile: path.join(__dirname, './webPlugins.css'),
 });
+
+app.use(compression());
 
 app.use('/v1', api);
 app.use('/', client);
